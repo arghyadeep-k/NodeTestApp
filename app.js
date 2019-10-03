55e094770e19e64f2f21aeb501cc7addaef9dbed
Arghyadeep Kundu <arghyadeep.k@gmail.com>
2019-10-03 06:46:44 +0000

    Added Console Log

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World!");
+    console.log("Listening on port 4000");
+    res.send("Hello World!");
 });
 
 app.listen(4000);
